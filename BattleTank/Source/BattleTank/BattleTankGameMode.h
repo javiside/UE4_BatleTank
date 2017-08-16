842795c52f86b01f4d2f1c2c39e1ce3fd3fc6a60
Francisco Javier Martinez <javi.mtz@hotmail.com>
2017-08-16 15:47:28 -0700

    BT02* Fixed GameMode (GameModeBase) Bug

---
+++ BattleTank/Source/BattleTank/BattleTankGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "BattleTankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattleTankGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
