c1accf9b88bab9f9e79afec88dce98b0a148a7ca
Francisco Javier Martinez <javi.mtz@hotmail.com>
2017-08-16 21:28:16 -0700

    BT09 Created Tank and TankPlayerControler Classes

---
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "Public/Tank.h"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATank* GetControlledTank() const;
+	
+	
+};
